d27fb9704ed949370a9f9e93eac8a6011ffb05b6
studentszO <169681673+studentszO@users.noreply.github.com>
2024-06-28 22:24:33 +0200

    Added: create a square with X number, X equals to the user's response in the prompt. Added: mouseover event to turn the background color of a box gray

--- project.js
+++ project.js
@@ -1,11 +1,20 @@
+let numberOfRows = prompt("How many rows do you want ?")
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < (numberOfRows * numberOfRows); i++) {
     const div = [];
     div[i] = document.createElement("div");
-    document.querySelector("#container").appendChild(div[i]);
+    document.querySelector("#container").appendChild(div[i]).style.width = (600
+         / numberOfRows) + "px";
+    div[i].style.aspectRatio = "1 / 1";
 }
 
+const divsGrid = document.querySelector("#container");
 
+divsGrid.addEventListener("mouseover", (event) => {
+    let target = event.target;
+
+    event.target.style.backgroundColor = "gray"
+})
 
 
 
