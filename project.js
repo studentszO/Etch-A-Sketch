6342378e3ac4658682f89839ab4aeac75b5b6597
studentszO <169681673+studentszO@users.noreply.github.com>
2024-06-28 23:11:51 +0200

    Make the for loop to create the grid into a function which help to call this function every time we want to change the size of the grid. Also add the code to remove all divs when the button is clicked

--- project.js
+++ project.js
@@ -1,14 +1,16 @@
 let numberOfRows = 16;
 
+function createGrid() { 
+    for (let i = 0; i < (numberOfRows * numberOfRows); i++) {
+        const div = [];
+        div[i] = document.createElement("div");
+        document.querySelector("#container").appendChild(div[i]).style.width = (600
+            / numberOfRows) + "px";
+        div[i].style.aspectRatio = "1 / 1";
+    }
+};
 
-
-for (let i = 0; i < (numberOfRows * numberOfRows); i++) {
-    const div = [];
-    div[i] = document.createElement("div");
-    document.querySelector("#container").appendChild(div[i]).style.width = (600
-         / numberOfRows) + "px";
-    div[i].style.aspectRatio = "1 / 1";
-}
+createGrid();
 
 const divsGrid = document.querySelector("#container");
 
@@ -21,7 +23,18 @@ divsGrid.addEventListener("mouseover", (event) => {
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
-    numberOfRows = prompt("How many rows do you want ?")
+    const container = document.querySelector("#container");
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+
+    numberOfRows = prompt("How many rows do you want ?");
+
+    if (numberOfRows > 100)
+        alert("100 is the MAXIMUM size!")
+
+    createGrid();
 });
 
 
